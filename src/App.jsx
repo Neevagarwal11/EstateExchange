92d3b3b2129fe6eb76ad2876ac2c57cb67fdd329
Neev Agarwal <agarwalneev2005@gmail.com>
2024-11-15 08:54:26 +0530

    created Pages & Route

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,26 @@
 import React from 'react'
+import {BrowserRouter , Routes,Route} from 'react-router-dom';
+import Home from './pages/Home'
+import SignIn from './pages/Signin'
+import Signup from './pages/Signup';
+import About from './pages/About';
+import Profile from './pages/Profile';
 
 function App() {
   return (
-    <div>App</div>
+  <BrowserRouter>
+  <Routes>
+    <Route path="/" element={<Home/>}></Route>
+    <Route path="/sign-in" element={<SignIn/>}></Route>
+    <Route path="/sign-up" element={<Signup/>}></Route>
+    <Route path="/about" element={<About/>}></Route>
+    <Route path="/profile" element={<Profile/>}></Route>
+
+  </Routes>
+
+  </BrowserRouter> 
   )
+  
 }
 
 export default App
---
+++ src/pages/About.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+function About() {
+  return (
+    <div>About ffe</div>
+  )
+}
+
+export default About
